a2250e73d4a9daf5a8f9f4fc2ff5c618083f3258
schacherl <schacherl@SCHACHERLNOTE>
2016-07-26 08:16:47 +0200

    List of customer and movies

---
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+	        var customers = GetCustomers();
+            return View(customers);
+        }
+
+		public ActionResult Details(int id)
+		{
+			var customer = GetCustomers().FirstOrDefault(x => x.Id == id);
+
+			if (customer == null)
+			{
+				return HttpNotFound();
+			}
+
+			return View(customer);
+		}
+
+	    private List<Customer> GetCustomers()
+	    {
+		    return new List<Customer>()
+		    {
+			    new Customer() {Id = 1, Name = "Jan Schacherl"},
+				new Customer() {Id = 2, Name = "Jakub Vostry"}
+			};
+	    }
+    }
+}
--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -11,46 +11,64 @@ namespace Vidly.Controllers
 {
 	public class MoviesController : Controller
 	{
-		// GET: Movies
-		public ActionResult Random()
+		//// GET: Movies
+		//public ActionResult Random()
+		//{
+		//	var movie = new Movie() { Name = "Shrek" };
+		//	var customers = new List<Customer>()
+		//	{
+		//		new Customer() {Name = "Customer1"},
+		//		new Customer() {Name = "Customer2"}
+		//	};
+
+		//	var viewModel = new RandomMovieViewMoel()
+		//	{
+		//		Movie = movie,
+		//		Customers = customers
+		//	};
+		//	//return View(movie);
+		//	//return Content("Hello Word dgdgg");			
+		//	//return HttpNotFound();
+		//	//return new EmptyResult();
+		//	//return RedirectToAction("Index", "Home", new {page = 1, sortby = "name"});
+
+		//	// nepouzivat, varianty pres string
+		//	//ViewData["Movie"] = movie;
+		//	// ViewBag.Movie = movie;
+
+
+		//	return View(viewModel);
+		//}
+
+
+
+		public ActionResult Index()
 		{
-			var movie = new Movie() { Name = "Shrek" };
-			var customers = new List<Customer>()
+			return View(GetMovies());
+		}
+
+
+		public List<Movie> GetMovies()
+		{
+			var movies = new List<Movie>()
 			{
-				new Customer() {Name = "Customer1"},
-				new Customer() {Name = "Customer2"}
+				new Movie() {Name = "Terminator 1"},
+				new Movie() {Name = "Rocky 2"}
 			};
-
-			var viewModel = new RandomMovieViewMoel()
-			{
-				Movie = movie,
-				Customers = customers
-			};
-			//return View(movie);
-			//return Content("Hello Word dgdgg");			
-			//return HttpNotFound();
-			//return new EmptyResult();
-			//return RedirectToAction("Index", "Home", new {page = 1, sortby = "name"});
-
-			// nepouzivat, varianty pres string
-			//ViewData["Movie"] = movie;
-			// ViewBag.Movie = movie;
-
-
-			return View(viewModel);
+			return movies;
 		}
 
 
-		public ActionResult Edit(int id)
-		{
-			return Content("id=" + id);
-		}
+		//public ActionResult Edit(int id)
+		//{
+		//	return Content("id=" + id);
+		//}
 
-		[Route("movies/released/{year}/{month}")]
-		public ActionResult ByReleaseDate(int year, int month)
-		{
-			return Content($"{year}/{month}");
-		}
+		//[Route("movies/released/{year}/{month}")]
+		//public ActionResult ByReleaseDate(int year, int month)
+		//{
+		//	return Content($"{year}/{month}");
+		//}
 
 
 	}
